20560fad8a54b0b8a5151c36e52959f0d4364525
Mukunthan <mukunthan.1255@gmail.com>
2025-10-10 12:09:46 +0530

    refactor UI

--- src/pages/patient/PatientAdmissionRecords.jsx
+++ src/pages/patient/PatientAdmissionRecords.jsx
@@ -7,12 +7,16 @@ import {
   BedIcon,
   CheckCircleIcon,
   CheckIcon,
+  ChevronLeftIcon,
   ClipboardCheckIcon,
   ClipboardListIcon,
   StethoscopeIcon,
 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const PatientAdmissionRecords = () => {
+  const navigate = new useNavigate();
+
   const admissionsData = [
     {
       id: "adm-002",
@@ -671,9 +675,29 @@ const PatientAdmissionRecords = () => {
 
   return (
     <div className="p-4">
-      {sortedAdmissions.map((admission) => (
-        <PatientAdmissionHistoryCard key={admission.id} admission={admission} />
-      ))}
+      <div className="flex items-center mb-4">
+        <button
+          className={`mr-2 w-8 h-8 flex items-center justify-center rounded-full       `}
+          onClick={() => navigate(-1)}
+          style={{
+            background: "linear-gradient(to bottom, #f0f4fa, #d5dde8)",
+            border: "1px solid rgba(0,0,0,0.2)",
+            boxShadow:
+              "0 1px 2px rgba(0,0,0,0.1), inset 0 1px 0 rgba(255,255,255,0.8)",
+          }}
+        >
+          <ChevronLeftIcon size={18} className="text-blue-700" />
+        </button>
+        <h2 className="text-xl text-blue-900 font-medium">Admission Records</h2>
+      </div>
+      <div className="rounded-xl overflow-hidden">
+        {sortedAdmissions.map((admission) => (
+          <PatientAdmissionHistoryCard
+            key={admission.id}
+            admission={admission}
+          />
+        ))}
+      </div>
     </div>
   );
 };
