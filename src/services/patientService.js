b0ea9f8fe64cb907a8ad49194e39a9c37d8347d6
Mukunthan <mukunthan.1255@gmail.com>
2025-10-30 10:04:47 +0530

    patient service

---
+++ src/services/patientService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000";
+
+export const fetchPatientById = async (id) => {
+  if (!id) {
+    console.warn("fetchPatientById called without an ID");
+    return null;
+  }
+
+  const response = await axios.get(`${BASE_URL}/api/patient/${id}`);
+  return response.data;
+};
