fa4a85218fd6b8b92bef2c4161cd7a724f1f79a0
Mukunthan <mukunthan.1255@gmail.com>
2025-12-10 15:40:37 +0530

    patient Profile comepnt

--- src/components/patient/PatientProfile.jsx
+++ src/components/patient/PatientProfile.jsx
@@ -1,23 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPatientById } from "../../services/patientService";
 import PersonContactInfo from "../PersonContactInfo";
 import PersonEmergencyContactInfo from "../PersonEmergencyContactInfo";
 import PersonProfileHeader from "../PersonProfileHeader";
 import PersonPersonalInformation from "../PersonPersonalInformation";
 import PatientProfileInsurance from "./PatientProfileInsurance";
-import { useParams } from "react-router-dom";
-import { userData } from "../../context/userAtom";
-import { useRecoilValue } from "recoil";
 
 export default function PatientProfile() {
   const { patientId } = useParams();
-  const userDataVal = useRecoilValue(userData);
-  console.log("patientId : " + patientId);
+
+  const {
+    data: userData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["studentData", patientId],
+    queryFn: () => fetchPatientById(patientId),
+    enabled: !!patientId,
+  });
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
 
   return (
     <div className="px-4 py-5 max-w-6xl mx-auto w-full">
       <div className="flex flex-col sm:flex-row gap-4 sm:gap-8">
         <div className="flex-1 sm:pb-6 ">
           {/* Profile Header */}
-          <PersonProfileHeader userDataVal={userDataVal} />
+          <PersonProfileHeader userDataVal={userData} />
         </div>
         <div className="flex-1 ">
           {/* Emergency Contact */}
@@ -28,11 +40,11 @@ export default function PatientProfile() {
       <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 mt-6">
         <div className="flex-1 ">
           {/* Personal Information */}
-          <PersonPersonalInformation userDataVal={userDataVal} />
+          <PersonPersonalInformation userDataVal={userData} />
         </div>
         <div className="flex-1 ">
           {/* Contact Information */}
-          <PersonContactInfo userDataVal={userDataVal} />
+          <PersonContactInfo userDataVal={userData} />
         </div>
       </div>
 
