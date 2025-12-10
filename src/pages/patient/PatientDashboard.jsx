f49dec8ccdf74efb383c24aefb91136f10cff9e5
Mukunthan <mukunthan.1255@gmail.com>
2025-12-10 15:29:28 +0530

    patient Dashboard compoennt

--- src/pages/patient/PatientDashboard.jsx
+++ src/pages/patient/PatientDashboard.jsx
@@ -1,20 +1,30 @@
-import React from "react";
 import PatientDashboardProfile from "../../components/patient/dashboard/PatientDashboardProfile";
 import { useParams } from "react-router-dom";
 import PatientDashboardServices from "../../components/patient/dashboard/PatientDashboardServices";
 import PatientDashboardNotification from "../../components/patient/dashboard/PatientDashboardNotification";
-import { userData } from "../../context/userAtom";
-import { useRecoilValue } from "recoil";
+import { fetchPatientById } from "../../services/patientService";
+import { useQuery } from "@tanstack/react-query";
 
 const PatientDashboard = () => {
-  const userDataVal = useRecoilValue(userData);
   const { patientId } = useParams();
+
+  const {
+    data: userData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["studentData", patientId],
+    queryFn: () => fetchPatientById(patientId),
+    enabled: !!patientId,
+  });
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
 
   return (
     <div className="px-2 sm:px-4 py-2 sm:py-5 max-w-6xl mx-auto">
-      <PatientDashboardProfile
-        userDataVal={userDataVal}
-      ></PatientDashboardProfile>
+      <PatientDashboardProfile userDataVal={userData}></PatientDashboardProfile>
       <PatientDashboardNotification></PatientDashboardNotification>
       <PatientDashboardServices
         onNavigate={(path) => console.log("Navigate to:", path)}
