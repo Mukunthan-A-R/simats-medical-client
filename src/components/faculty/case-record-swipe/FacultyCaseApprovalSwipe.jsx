6e71b30da646d27b72685934610e80cb9b89bb65
Mukunthan <mukunthan.1255@gmail.com>
2025-12-15 11:00:44 +0530

    Adding Faculty Case Approval Header

--- src/components/faculty/case-record-swipe/FacultyCaseApprovalSwipe.jsx
+++ src/components/faculty/case-record-swipe/FacultyCaseApprovalSwipe.jsx
@@ -6,6 +6,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { fetchDoctorProcedureCaseRecords } from "../../../services/doctorProcedureCaseRecord";
 import PatientCaseApprovalCard from "../PatientCaseApprovalCard";
 import FullScreenPatientCard from "./FullScreenPatientCard";
+import PageHeader from "../../header/PageHeader";
 
 const FacultyCaseApprovalSwipe = () => {
   const { facultyId: doctorId } = useParams();
@@ -28,35 +29,36 @@ const FacultyCaseApprovalSwipe = () => {
   const next = () => index < patients.length - 1 && setIndex(index + 1);
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100 relative">
-      {/* Swipe container with limited width */}
-      <SwipeWrapper onLeft={next} onRight={prev}>
-        <div className="flex items-center justify-center">
-          <div>
-            <FullScreenPatientCard patient={patients[index]} />
+    <div className="bg-gray-100 h-screen p-4">
+      <PageHeader title={"Case Records Approval"} />
+      <div className="h-full flex items-center justify-center relative">
+        {/* Swipe container with limited width */}
+        <SwipeWrapper onLeft={next} onRight={prev}>
+          <div className="flex items-center justify-center">
+            <div>
+              <FullScreenPatientCard patient={patients[index]} />
+            </div>
           </div>
-        </div>
-      </SwipeWrapper>
-
-      {/* Left Arrow */}
-      {index > 0 && (
-        <button
-          onClick={prev}
-          className="absolute left-2 top-1/2 -translate-y-1/2 p-2 rounded-full bg-white shadow hover:bg-gray-100"
-        >
-          <ChevronLeftIcon size={24} />
-        </button>
-      )}
-
-      {/* Right Arrow */}
-      {index < patients.length - 1 && (
-        <button
-          onClick={next}
-          className="absolute right-2 top-1/2 -translate-y-1/2 p-2 rounded-full bg-white shadow hover:bg-gray-100"
-        >
-          <ChevronRightIcon size={24} />
-        </button>
-      )}
+        </SwipeWrapper>
+        {/* Left Arrow */}
+        {index > 0 && (
+          <button
+            onClick={prev}
+            className="absolute left-2 top-1/2 -translate-y-1/2 p-2 rounded-full bg-white shadow hover:bg-gray-100"
+          >
+            <ChevronLeftIcon size={24} />
+          </button>
+        )}
+        {/* Right Arrow */}
+        {index < patients.length - 1 && (
+          <button
+            onClick={next}
+            className="absolute right-2 top-1/2 -translate-y-1/2 p-2 rounded-full bg-white shadow hover:bg-gray-100"
+          >
+            <ChevronRightIcon size={24} />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
